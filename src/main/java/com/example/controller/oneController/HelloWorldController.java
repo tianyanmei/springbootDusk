97a7c6f17da04a6e57075c114080daa656dfc339
tianyanmei <a168209@sina.cn>
2019-07-24 15:17:13 +0800

    test

--- src/main/java/com/example/controller/oneController/HelloWorldController.java
+++ src/main/java/com/example/controller/oneController/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.controller.oneController;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,5 +20,10 @@ public class HelloWorldController {
     }
 
 
+     //http://localhost:8080/hello/hello2?id=2
+    @RequestMapping(value="hello2")
+    public String hello2(@RequestParam(value = "id") String id){
+       return  id;
+    }
 
 }
